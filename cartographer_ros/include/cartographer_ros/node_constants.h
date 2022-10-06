d3182376681326ccb54df0944c2fb28cf598f8e1
ivan <ivan.villaverde@tecnalia.com>
2022-10-06 17:24:32 +0200

    node_main compiles

--- cartographer_ros/include/cartographer_ros/node_constants.h
+++ cartographer_ros/include/cartographer_ros/node_constants.h
@@ -12,31 +12,31 @@ namespace cartographer_ros
 {
 
 // Default topic names; expected to be remapped as needed.
-constexpr char kLaserScanTopic[] = "scan";
-constexpr char kMultiEchoLaserScanTopic[] = "echoes";
-constexpr char kPointCloud2Topic[] = "points2";
-constexpr char kImuTopic[] = "imu";
-constexpr char kOdometryTopic[] = "odom";
-constexpr char kNavSatFixTopic[] = "fix";
-constexpr char kLandmarkTopic[] = "landmark";
-constexpr char kFinishTrajectoryServiceName[] = "finish_trajectory";
-constexpr char kOccupancyGridTopic[] = "occupancy_grid";
-constexpr char kScanMatchedPointCloudTopic[] = "scan_matched_points2";
-constexpr char kSubmapListTopic[] = "submap_list";
-constexpr char kSubmapQueryServiceName[] = "submap_query";
-constexpr char kTrajectoryQueryServiceName[] = "trajectory_query";
-constexpr char kLoadStateServiceName[] = "load_state";
-constexpr char kWriteStateServiceName[] = "write_state";
-constexpr char kGetTrajectoryStatesServiceName[] = "get_trajectory_states";
-constexpr char kReadMetricsServiceName[] = "read_metrics";
-constexpr char kMapDataTopic[] = "map_data";
-constexpr char kTrajectoryNodeListTopic[] = "trajectory_node_list";
-constexpr char kLandmarkPosesListTopic[] = "landmark_poses_list";
-constexpr char kConstraintListTopic[] = "constraint_list";
-constexpr double kConstraintPublishPeriodSec = 0.5;
-constexpr double kTopicMismatchCheckDelaySec = 3.0;
+const std::string kLaserScanTopic = "scan";
+const std::string kMultiEchoLaserScanTopic = "echoes";
+const std::string kPointCloud2Topic = "points2";
+const std::string kImuTopic = "imu";
+const std::string kOdometryTopic = "odom";
+const std::string kNavSatFixTopic = "fix";
+const std::string kLandmarkTopic = "landmark";
+const std::string kFinishTrajectoryServiceName = "finish_trajectory";
+const std::string kOccupancyGridTopic = "occupancy_grid";
+const std::string kScanMatchedPointCloudTopic = "scan_matched_points2";
+const std::string kSubmapListTopic = "submap_list";
+const std::string kSubmapQueryServiceName = "submap_query";
+const std::string kTrajectoryQueryServiceName = "trajectory_query";
+const std::string kLoadStateServiceName = "load_state";
+const std::string kWriteStateServiceName = "write_state";
+const std::string kGetTrajectoryStatesServiceName = "get_trajectory_states";
+const std::string kReadMetricsServiceName = "read_metrics";
+const std::string kMapDataTopic = "map_data";
+const std::string kTrajectoryNodeListTopic = "trajectory_node_list";
+const std::string kLandmarkPosesListTopic = "landmark_poses_list";
+const std::string kConstraintListTopic = "constraint_list";
+const double kConstraintPublishPeriodSec = 0.5;
+const double kTopicMismatchCheckDelaySec = 3.0;
 
-constexpr int kInfiniteSubscriberQueueSize = 0;
+const int kInfiniteSubscriberQueueSize = 0;
 
 // For multiple topics adds numbers to the topic name and returns the list.
 std::vector<std::string> ComputeRepeatedTopicNames(const std::string& topic, int num_topics);
--- cartographer_ros/src/node_main.cc
+++ cartographer_ros/src/node_main.cc
@@ -4,34 +4,14 @@
 #include "cartographer_ros/node_options.h"
 #include "cartographer_ros/ros_log_sink.h"
 #include "gflags/gflags.h"
-#include "tf2_ros/transform_listener.h"
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/transform_listener.h>
 
-template <typename T> T get_param_or_throw(const std::string& param_name)
-{
-    if (ros::param::has(param_name))
-    {
-        T param_val;
-        if (ros::param::get(param_name, param_val))
-        {
-            return param_val;
-        }
-    }
-    throw std::runtime_error("Missing required param: " + param_name);
-}
-
-template <typename T> T get_param_with_default_warn(const std::string& param_name, const T& default_val)
-{
-    if (ros::param::has(param_name))
-    {
-        T param_val;
-        if (ros::param::get(param_name, param_val))
-        {
-            return param_val;
-        }
-    }
-    ROS_WARN_STREAM("Using default value for " << param_name << "=" << default_val);
-    return default_val;
-}
+DEFINE_string(configuration_directory, "",
+              "First directory in which configuration files are searched, "
+              "second is always the Cartographer installation to allow "
+              "including files from there.");
+DEFINE_bool(collect_metrics, false, "Collect metrics or not.");
 
 namespace cartographer_ros
 {
@@ -40,20 +20,37 @@ namespace
 
 void Run()
 {
-    const std::string configuration_directory = get_param_or_throw<std::string>("~configuration_directory");
-    const bool collect_metrics = get_param_with_default_warn<bool>("~collect_metrics", false);
+    std::string temp_configuration_directory;
+    bool temp_collect_metrics;
 
-    constexpr double kTfBufferCacheTimeInSeconds = 60.;
-    auto tf_buffer = std::make_shared<tf2_ros::Buffer>(::ros::Duration(kTfBufferCacheTimeInSeconds));
-    tf2_ros::TransformListener tf(*tf_buffer);
+    // https://stackoverflow.com/questions/54295618/how-to-know-if-a-gflag-was-provided-in-the-command-line
+    if (!gflags::GetCommandLineFlagInfoOrDie("configuration_directory").is_default)
+    {
+            temp_configuration_directory = FLAGS_configuration_directory;
+    }
+    else 
+    {
+        throw std::runtime_error("Missing required param: configuration_directory");
+    }
+    if (!gflags::GetCommandLineFlagInfoOrDie("collect_metrics").is_default)
+    {
+            temp_collect_metrics = FLAGS_collect_metrics;
+    }
+    else 
+    {
+        temp_collect_metrics = false;
+    }
+
+    const std::string configuration_directory = temp_configuration_directory;
+    const bool collect_metrics = temp_collect_metrics;
 
     NodeOptions node_options;
     TrajectoryOptions trajectory_options;
     std::tie(node_options, trajectory_options) = LoadOptions(configuration_directory, "cartographer.lua");
 
-    Node node(node_options, trajectory_options, tf_buffer, collect_metrics);
+    auto node = std::make_shared<cartographer_ros::Cartographer>(node_options, trajectory_options, collect_metrics);
 
-    ::ros::spin();
+    rclcpp::spin(node);
 }
 
 }  // namespace
@@ -61,13 +58,12 @@ void Run()
 
 int main(int argc, char** argv)
 {
+    ::rclcpp::init(argc, argv);
+
     google::InitGoogleLogging(argv[0]);
     google::ParseCommandLineFlags(&argc, &argv, true);
 
-    ::ros::init(argc, argv, "mapper");
-    ::ros::start();
-
     cartographer_ros::ScopedRosLogSink ros_log_sink;
     cartographer_ros::Run();
-    ::ros::shutdown();
+    ::rclcpp::shutdown();
 }
