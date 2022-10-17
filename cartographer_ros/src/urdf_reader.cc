286cd78d334c5cb6e8e3552b930bbfabebc58e7c
ivan <ivan.villaverde@tecnalia.com>
2022-10-17 14:39:12 +0200

    assets_writer compiles

--- cartographer_ros/src/urdf_reader.cc
+++ cartographer_ros/src/urdf_reader.cc
@@ -22,39 +22,34 @@
 #include "cartographer_ros/msg_conversion.h"
 #include "urdf/model.h"
 
-namespace cartographer_ros
-{
+namespace cartographer_ros {
 
-std::vector<geometry_msgs::msg::TransformStamped> ReadStaticTransformsFromUrdf(const std::string& urdf_filename,
-                                                                          tf2_ros::Buffer* const tf_buffer)
-{
-    urdf::Model model;
-    CHECK(model.initFile(urdf_filename));
-#if URDFDOM_HEADERS_HAS_SHARED_PTR_DEFS
-    std::vector<urdf::LinkSharedPtr> links;
-#else
-    std::vector<boost::shared_ptr<urdf::Link>> links;
-#endif
-    model.getLinks(links);
-    std::vector<geometry_msgs::msg::TransformStamped> transforms;
-    for (const auto& link : links)
-    {
-        if (!link->getParent() || link->parent_joint->type != urdf::Joint::FIXED)
-        {
-            continue;
-        }
-
-        const urdf::Pose& pose = link->parent_joint->parent_to_joint_origin_transform;
-        geometry_msgs::msg::TransformStamped transform;
-        transform.transform = ToGeometryMsgTransform(cartographer::transform::Rigid3d(
-            Eigen::Vector3d(pose.position.x, pose.position.y, pose.position.z),
-            Eigen::Quaterniond(pose.rotation.w, pose.rotation.x, pose.rotation.y, pose.rotation.z)));
-        transform.child_frame_id = link->name;
-        transform.header.frame_id = link->getParent()->name;
-        tf_buffer->setTransform(transform, "urdf", true /* is_static */);
-        transforms.push_back(transform);
+std::vector<geometry_msgs::msg::TransformStamped> ReadStaticTransformsFromUrdf(
+    const std::string& urdf_filename, std::shared_ptr<tf2_ros::Buffer> tf_buffer) {
+  urdf::Model model;
+  CHECK(model.initFile(urdf_filename));
+  std::vector<urdf::LinkSharedPtr> links;
+  model.getLinks(links);
+  std::vector<geometry_msgs::msg::TransformStamped> transforms;
+  for (const auto& link : links) {
+    if (!link->getParent() || link->parent_joint->type != urdf::Joint::FIXED) {
+      continue;
     }
-    return transforms;
+
+    const urdf::Pose& pose =
+        link->parent_joint->parent_to_joint_origin_transform;
+    geometry_msgs::msg::TransformStamped transform;
+    transform.transform =
+        ToGeometryMsgTransform(cartographer::transform::Rigid3d(
+            Eigen::Vector3d(pose.position.x, pose.position.y, pose.position.z),
+            Eigen::Quaterniond(pose.rotation.w, pose.rotation.x,
+                               pose.rotation.y, pose.rotation.z)));
+    transform.child_frame_id = link->name;
+    transform.header.frame_id = link->getParent()->name;
+    tf_buffer->setTransform(transform, "urdf", true /* is_static */);
+    transforms.push_back(transform);
+  }
+  return transforms;
 }
 
 }  // namespace cartographer_ros
